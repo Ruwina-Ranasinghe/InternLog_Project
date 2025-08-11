ccea2ac5f4666befaa7af5d67df69c66a6032059
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2025-08-11 15:41:11 +0530

    Remove admin model and add task model

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ITask extends Document {
+    user: mongoose.Types.ObjectId;
+    task_name: string;
+    status: string;
+    description: string;
+    date: Date;
+}
+
+const taskSchema = new Schema<ITask>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "user is required"]
+    },
+    task_name: {
+        type: Schema.Types.String,
+        required: [true, "task_name is required"]
+    },
+    status: {
+        type: Schema.Types.String,
+        enum: ["pending", "in-progress", "completed"],
+        default: "pending"
+    },
+    description: {
+        type: Schema.Types.String
+    },
+    date: {
+        type: Schema.Types.Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model<ITask>("Task", taskSchema);
