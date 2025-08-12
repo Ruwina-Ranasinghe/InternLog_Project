912ac4305d2beedea7a387e515c3e8a217a4ce51
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-12 11:29:20 +0530

    Modify server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './util/db';
 import path from "node:path";
+import authRoutes from './routes/auth.routes';
 
 dotenv.config({ path: path.resolve(__dirname, "../config/.env") });
 connectDB();
@@ -11,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+app.use('/api/login', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
