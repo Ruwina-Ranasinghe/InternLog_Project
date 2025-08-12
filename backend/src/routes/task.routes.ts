416d57dcbdc72cd3bcde8fb40a7d510d67add58d
Imesha Jayaweera <imeshajayaweera9@gmail.com>
2025-08-12 10:15:39 +0530

    create task routes

---
+++ backend/src/routes/task.routes.ts
@@ -0,0 +1,11 @@
+import { Router} from "express";
+import * as taskController from "../controllers/task.controller";
+
+const taskRouter = Router();
+
+taskRouter.get('/', taskController.getAllTasks);
+taskRouter.post('/create-task',taskController.createTask);
+taskRouter.put('/update-task/:id', taskController.updateTask);
+taskRouter.delete('/delete-task/:id', taskController.deleteTask);
+
+export default taskRouter;
