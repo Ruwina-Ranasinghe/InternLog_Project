96e849f9931a4f487e7d9c1f2855d01a4e64be30
Imesha Jayaweera <imeshajayaweera9@gmail.com>
2025-08-12 16:24:47 +0530

    final update task routes middleware usage

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,15 +1,15 @@
 import { Router} from "express";
+import {authMiddleware} from "../middleware/auth.middleware";
 import * as taskController from "../controllers/task.controller";
-import { authMiddleware } from "../middleware/auth.middleware";
 
 
 const taskRouter = Router();
 
-taskRouter.get('/',authMiddleware("user"),taskController.getAllTasks);
-taskRouter.post('/create-task',authMiddleware("user"),taskController.createTask);
-taskRouter.put('/update-task/:id',authMiddleware("user"), taskController.updateTask);
-taskRouter.delete('/delete-task/:id',authMiddleware("user"), taskController.deleteTask);
+taskRouter.get('/', authMiddleware("admin"),taskController.getAllTasks);
 
-taskRouter.get("/admin/tasks", authMiddleware("admin"), taskController.getAllTasksForAdmin);
+taskRouter.get('/get-tasks',authMiddleware("user"),taskController.getUserTasks);
+taskRouter.post('/create-task',authMiddleware("user"),taskController.createTask);
+taskRouter.put('/update-task/:id', authMiddleware("user"), taskController.updateTask);
+taskRouter.delete('/delete-task/:id', authMiddleware("user"), taskController.deleteTask);
 
 export default taskRouter;
