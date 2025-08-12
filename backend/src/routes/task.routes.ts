3c5a554186aff95069d371831f3e44ed8013f431
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2025-08-12 16:57:21 +0530

    Add auth middleware to task routes

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,12 +1,15 @@
 import { Router} from "express";
+import {authMiddleware} from "../middleware/auth.middleware";
 import * as taskController from "../controllers/task.controller";
 
 
 const taskRouter = Router();
 
-taskRouter.get('/',taskController.getAllTasks);
-taskRouter.post('/create-task',taskController.createTask);
-taskRouter.put('/update-task/:id', taskController.updateTask);
-taskRouter.delete('/delete-task/:id', taskController.deleteTask);
+taskRouter.get('/', authMiddleware("admin"),taskController.getAllTasks);
+
+taskRouter.get('/get-tasks',authMiddleware("user"),taskController.getUserTasks);
+taskRouter.post('/create-task',authMiddleware("user"),taskController.createTask);
+taskRouter.put('/update-task/:id', authMiddleware("user"), taskController.updateTask);
+taskRouter.delete('/delete-task/:id', authMiddleware("user"), taskController.deleteTask);
 
 export default taskRouter;
