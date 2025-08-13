958bc984235d622f6e80ce7e8c021acd163d1b2e
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-13 20:52:59 +0530

    Update layout and responsiveness of Header

--- frontend/src/components/webHeader.tsx
+++ frontend/src/components/webHeader.tsx
@@ -1,32 +1,30 @@
-import { Autocomplete, Burger, Group } from '@mantine/core';
+import { Burger, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { Link } from 'react-router-dom';
 import logo from '../assets/logo1.png';
 
-export function WebHeader() {
+const WebHeader = () => {
   const [opened, { toggle }] = useDisclosure(false);
 
   return (
     <header
-          className="w-full border-b border-gray-200 dark:border-gray-700 px-4"
-          style={{ backgroundColor: '#D3B5F8' }}
-        >
+      className="fixed top-0 left-0 w-full border-b border-gray-200 dark:border-gray-700 px-4 z-50"
+      style={{ backgroundColor: '#D3B5F8', borderBottom: '2px solid #830999'  }}
+    >
       <div className="flex justify-between items-center h-14 sm:h-16 md:h-20 lg:h-24">
         <Group>
           <Burger opened={opened} onClick={toggle} size="sm" className="sm:hidden" />
-          <div className="flex items-center ml-4">
-                      <img
-                        src={logo}
-                        alt="Company Logo"
-                        className="h-8 w-auto sm:h-10 md:h-12 lg:h-14 object-contain"
-                        style={{ maxWidth: '200px' }}
-                      />
-                    </div>
-
+          <div className="flex items-center -mt-5 sm:mt-0 sm:ml-2 lg:ml-4">
+            <img
+              src={logo}
+              alt="Company Logo"
+              className="h-8 w-auto sm:h-10 md:h-12 lg:h-14 object-contain"
+              style={{ maxWidth: '200px' }}
+            />
+          </div>
         </Group>
-
-
       </div>
     </header>
   );
-}
+};
+
+export default WebHeader;
