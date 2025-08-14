99f65fc7b42db66f4d83833fcaea952b3aac12f5
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-14 23:04:34 +0530

    Update user dashboard and task pages layout

--- frontend/src/pages/user/editTask/index.tsx
+++ frontend/src/pages/user/editTask/index.tsx
@@ -10,7 +10,7 @@ const EditTask = () => {
             <div className="flex">
                 <SidebarUser userName={"USER"} userEmail={"user@gmail.com"} />
                 <main className="flex-1 ml-0 md:ml-64 mt-14 sm:mt-16 md:mt-20 lg:mt-24 p-4 overflow-y-auto">
-                    <div className="lg:p-10 lg:pt-20 p-3 pt-10">
+                    <div className="lg:pt-1 p-1 pt-8">
                         <div>
                             <EditTaskForm />
                         </div>
--- frontend/src/pages/user/userDashboard/index.tsx
+++ frontend/src/pages/user/userDashboard/index.tsx
@@ -1,19 +1,27 @@
+import TaskStatusChart from "../../../components/pieChart";
 import SidebarUser from "../../../components/sidebarUser";
+import TaskStatusCards from "../../../components/taskStatusCard";
 import WebHeader from "../../../components/webHeader";
 
-const UserDashboard = () => {
+const CreateTask = () => {
+    const completedValue = 140;
+    const inProgressedValue = 100;
+    const todoValue = 30;
+
     return (
                 
         <div>
             <WebHeader/>
             <div className="flex">
-                <SidebarUser userName={"USER"} userEmail={"user@gmail.com"}/>
+                <SidebarUser userName={"USER"} userEmail={"user@gmail.com"} />
                 <main className="flex-1 ml-0 md:ml-64 mt-14 sm:mt-16 md:mt-20 lg:mt-24 p-4 overflow-y-auto">
-                    <div className="lg:p-10 lg:pt-50 p-3 pt-10">
-                        <div className="pb-7 text-2xl font-bold" style={{color: '#1f2937'}}>
-                            User Dashboard
+                    <div className="lg:pt-3 p-10 pt-9">
+                        <div>
+                            <TaskStatusChart completed={completedValue} inProgress={inProgressedValue} todo={todoValue} height={240} innerRadius={80} outerRadius={110}/>
                         </div>
-                        
+                        <div className="mt-9">
+                            <TaskStatusCards completed={completedValue} inProgress={inProgressedValue} todo={todoValue} />
+                        </div>
                     </div>
                     
                 </main>
@@ -24,4 +32,4 @@ const UserDashboard = () => {
     )
 }
 
-export default UserDashboard;
+export default CreateTask;
