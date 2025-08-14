2fa370f42ef5407b020219218c64303cbdc47c75
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-14 12:57:45 +0530

    Add user routes and 404 page to App router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminLog from './pages/admin/adminLog';
 import AdminDashboard from './pages/admin/adminDashboard';
+import ViewAllTasks from './pages/user/viewAllTasks';
+import UserDashboard from './pages/user/userDashboard';
+import CreateTask from './pages/user/createTask';
+import EditTask from './pages/user/editTask';
+import { NotFound } from './pages/404/errorPage';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<AdminLog />} /> 
+               
+                <Route path="/" element={<ViewAllTasks />} /> 
+
+                <Route path="/user-dashboard" element={<UserDashboard />} /> 
+                <Route path="/create-task" element={<CreateTask />} /> 
+                <Route path="/view-all-tasks" element={<ViewAllTasks />} /> 
+                <Route path="/edit-task" element={<EditTask />} /> 
+                
+
+                <Route path="/admin-log" element={<AdminLog />} /> 
+                <Route path="/admin-dashboard" element={<AdminDashboard />} /> 
+                <Route path="*" element={<NotFound />} /> 
             </Routes>
         </Router>
     );
